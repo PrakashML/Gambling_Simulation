ebcdfb95fa55c88038a08aebad9b30b1fdc9a37f
PrakashML <prakashkrishnan08@gmail.com>
2024-01-25 12:08:58 +0530

    game starts

--- Gambler/src/com/workshop/gambling/Gambler.java
+++ Gambler/src/com/workshop/gambling/Gambler.java
@@ -1,5 +1,43 @@
 package com.workshop.gambling;
+import java.util.Random;
 
 public class Gambler {
-    
+    private int stake;
+    private int betAmount;
+
+    public Gambler(int initialStake, int betAmount) {
+        this.stake = initialStake;
+        this.betAmount = betAmount;
+    }
+
+    public void playGame() {
+        boolean win = new Random().nextBoolean();
+
+        if (win) {
+            stake += betAmount;
+            System.out.println("You won! Current stake: $" + stake);
+        } else {
+            stake -= betAmount;
+            System.out.println("You lost. Current stake: $" + stake);
+        }
+    }
+
+    public void simulateGames(int numGames) {
+        for (int i = 0; i < numGames; i++) {
+            if (stake <= 0) {
+                System.out.println("Out of money. Game over.");
+                break;
+            }
+
+            playGame();
+        }
+    }
+
+    public static void main(String[] args) {
+        Gambler gambler = new Gambler(100, 1);
+        gambler.simulateGames(20);
+    }
+
+
 }
+
