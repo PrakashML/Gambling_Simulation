e2d186e306dc35512a0c348766741c60d8beeb5b
PrakashML <prakashkrishnan08@gmail.com>
2024-01-29 01:07:12 +0530

    After 20 days of playing everyday would like to know the total amount of wins and losses

--- Gambler/src/com/workshop1/gambler/gambler.java
+++ Gambler/src/com/workshop1/gambler/gambler.java
@@ -8,6 +8,8 @@ public class gambler {
     int stakecalc = 100;
     int bet = 1;
     int numgames = 0;
+    int tot_win = 0;
+    int tot_lost = 0;
     public gambler(){
         this.stake = stake;
         this.bet = bet;
@@ -16,33 +18,36 @@ public class gambler {
         boolean win = new Random().nextBoolean();
         if(win){
             stakecalc+=bet;
+            tot_win++;
             System.out.println("You Won! Current stake: "+ stakecalc);
         }
         else{
             stakecalc-=bet;
+            tot_lost++;
             System.out.println("You lose! Current stake: "+ stakecalc);
         }
     }
 
     public void simulategame(int n){
-        for(int i=0;i<n;i++){
-            numgames+=1;
+        for(int i=1;i<=n;i++){
+            System.out.println("\nDay: " + i);
             if(stake <= 0){
                 System.out.println("Out of money, Game over");
                 break;
             }
-            game();
-            if(stakecalc<=0.5*stake){
-                System.out.println("Resigning for the Day. Current stake: " + stakecalc);
-                System.out.println("No of games played for the day: "+ numgames);
-                break;
+            while(stakecalc>0 && stakecalc>=0.5*stake){
+                game();
             }
+            stakecalc = stake;
         }
+        System.out.println("\nOverall results after 20 days: ");
+        System.out.println("Total Wins: " + tot_win);
+        System.out.println("Total lose: " + tot_lost);
     }
 
     public static void main(String[] args) {
         gambler g1 = new gambler();
-        g1.simulategame(Integer.MAX_VALUE);
+        g1.simulategame(20);
 
     }
 }
