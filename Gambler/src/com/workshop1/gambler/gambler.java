88cc8bfc2d4be8f4ae1c68f32dcb31bf86cb34d6
PrakashML <prakashkrishnan08@gmail.com>
2024-01-28 22:36:34 +0530

    gambler starts with a stake of 00 everyday and bet  every game

--- Gambler/src/com/workshop1/gambler/gambler.java
+++ Gambler/src/com/workshop1/gambler/gambler.java
@@ -1,8 +1,28 @@
 package com.workshop1.gambler;
 
-public class gambler {
-    
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class gambler {
+    int stake = 100;
+    int bet = 1;
+    public gambler(){
+        this.stake = stake;
+        this.bet = bet;
+    }
+    public void game(){
+        boolean win = new Random().nextBoolean();
+        if(win){
+            stake+=bet;
+            System.out.println("You Won!, Your current stake is: "+ stake);
+        }
+        else{
+            stake-=bet;
+            System.out.println("You lost!, Your current stake is: "+ stake);
+        }
+    }
+
+    public static void main(String[] args) {
+        gambler g1 = new gambler();
+        g1.game();
     }
 }
