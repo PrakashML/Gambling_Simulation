6a46719c468156378e2794b49629affc11aa22e0
PrakashML <prakashkrishnan08@gmail.com>
2024-01-28 23:17:22 +0530

    Gambler would start with a stake of 100 dollars every day and bet for one dollar every game

--- Gambler/src/com/workshop1/gambler/gambler.java
+++ Gambler/src/com/workshop1/gambler/gambler.java
@@ -9,20 +9,11 @@ public class gambler {
         this.stake = stake;
         this.bet = bet;
     }
-    public void game(){
-        boolean win = new Random().nextBoolean();
-        if(win){
-            stake+=bet;
-            System.out.println("You Won!, Your current stake is: "+ stake);
-        }
-        else{
-            stake-=bet;
-            System.out.println("You lost!, Your current stake is: "+ stake);
-        }
-    }
+
 
     public static void main(String[] args) {
         gambler g1 = new gambler();
-        g1.game();
+        System.out.println("Stake for each day: " + g1.stake);
+        System.out.println("Bet for each game :" + g1.bet);
     }
 }
