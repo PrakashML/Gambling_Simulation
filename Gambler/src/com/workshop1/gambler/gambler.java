c24f6177b990eabca92ae03fa230c8b8f8cb6455
PrakashML <prakashkrishnan08@gmail.com>
2024-01-29 00:50:16 +0530

    if won or lost 50% of the stake, would resign for the day

--- Gambler/src/com/workshop1/gambler/gambler.java
+++ Gambler/src/com/workshop1/gambler/gambler.java
@@ -1,10 +1,13 @@
 package com.workshop1.gambler;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class gambler {
     int stake = 100;
+    int stakecalc = 100;
     int bet = 1;
+    int numgames = 0;
     public gambler(){
         this.stake = stake;
         this.bet = bet;
@@ -12,18 +15,34 @@ public class gambler {
     public void game(){
         boolean win = new Random().nextBoolean();
         if(win){
-            stake+=bet;
-            System.out.println("You Won!, Your current stake is: "+ stake);
+            stakecalc+=bet;
+            System.out.println("You Won! Current stake: "+ stakecalc);
         }
         else{
-            stake-=bet;
-            System.out.println("You lost!, Your current stake is: "+ stake);
+            stakecalc-=bet;
+            System.out.println("You lose! Current stake: "+ stakecalc);
         }
     }
 
+    public void simulategame(int n){
+        for(int i=0;i<n;i++){
+            numgames+=1;
+            if(stake <= 0){
+                System.out.println("Out of money, Game over");
+                break;
+            }
+            game();
+            if(stakecalc<=0.5*stake){
+                System.out.println("Resigning for the Day. Current stake: " + stakecalc);
+                System.out.println("No of games played for the day: "+ numgames);
+                break;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         gambler g1 = new gambler();
-        g1.game();
+        g1.simulategame(Integer.MAX_VALUE);
+
     }
 }
