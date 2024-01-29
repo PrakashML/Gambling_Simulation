fae9b8cd72b6bf9c4de46dcb7aa61f474b08bc43
PrakashML <prakashkrishnan08@gmail.com>
2024-01-29 12:16:57 +0530

    each month would like to know the days won and lost and by how much

--- Gambler/src/com/workshop1/gambler/gambler.java
+++ Gambler/src/com/workshop1/gambler/gambler.java
@@ -3,17 +3,6 @@ package com.workshop1.gambler;
 import java.util.Random;
 import java.util.Scanner;
 public class gambler {
-    int stake;
-    int bet;
-    int numgame;
-
-
-    public gambler() {
-        this.stake = 100;
-        this.bet = 1;
-        this.numgame = 10;
-    }
-
     public static Boolean game(){
         if (Math.random()<0.5){
             return true;
@@ -23,34 +12,50 @@ public class gambler {
         }
     }
 
-    public static void Simulategame(int stake, int bet, int numgame){
-        String result;
-        for (int i=0; i<numgame; i++) {
-            if (game()) {
-                result = "win";
-                stake += bet;
+    private static void simulateGambling(int initialStake, int betAmount, int noOfDays, int noOfMonths, int numgame){
+        for (int month = 1; month <= noOfMonths; month++) {
+            int totalAmountWonOrLost = 0;
+            int totalDaysWon = 0;
+            int totalDaysLost = 0;
+            for (int day = 1; day <= noOfDays; day++) {
+                int currentStake = initialStake;
 
-            } else {
-                result = "lost";
-                stake -= bet;
+                while (currentStake > 50 && currentStake < 150) {
+                    for ( int game = 1; game <= numgame; game++){
+                        if (game()) {
+                            currentStake += betAmount;
+                        }
+                        else {
+                            currentStake -= betAmount;
+                        }
+                    }
+                }
+                int amountWonOrLost = currentStake - initialStake;
+                totalAmountWonOrLost += amountWonOrLost;
+
+                if (amountWonOrLost > 0) {
+                    totalDaysWon++;
+                }
+                else{
+                    totalDaysLost++;
+                }
+                System.out.println("Month " + month + " Day " + day + ": Amount Won or Lost - $ " + amountWonOrLost);
+
             }
-            if (stake <= 50 || stake >= 150){
-                System.out.println("Not able to Play furthur");
-                break;
-            }
-
-
-            System.out.println("result: " + result);
-            System.out.println("stakeValue : " + stake);
+            System.out.println("Month " + month + " : Total amount won or lost - $" + totalAmountWonOrLost);
+            System.out.println("Month " + month + " : Total Days Won - " + totalDaysWon);
+            System.out.println("Month " + month + " : Total Days Lost - " + totalDaysLost);
         }
     }
 
 
     public static void main(String[] args) {
-        gambler g1 = new gambler();
-        g1.Simulategame(100, 1, 20);
-        System.out.println(g1);
-
+        int initialStake = 100;
+        int betAmount = 1;
+        int noOfDays = 20;
+        int noOfMonths = 12;
+        int noOfGames = 100;
+        simulateGambling(initialStake, betAmount, noOfDays, noOfMonths, noOfGames);
 
     }
 }
